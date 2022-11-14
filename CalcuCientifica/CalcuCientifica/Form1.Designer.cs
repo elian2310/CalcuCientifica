e4b87500c3780014291c70aee03b3d13643453a1
elian2310 <elianpacheco23.10@gmail.com>
2022-11-14 00:46:19 -0400

    Added functions

--- CalcuCientifica/CalcuCientifica/Form1.Designer.cs
+++ CalcuCientifica/CalcuCientifica/Form1.Designer.cs
@@ -29,24 +29,208 @@ namespace CalcuCientifica
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.tb2 = new System.Windows.Forms.TextBox();
+            this.tbRes = new System.Windows.Forms.TextBox();
+            this.btnSuma = new System.Windows.Forms.Button();
+            this.btnResta = new System.Windows.Forms.Button();
+            this.btnMult = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.btnSin = new System.Windows.Forms.Button();
+            this.btnCos = new System.Windows.Forms.Button();
+            this.btnTan = new System.Windows.Forms.Button();
+            this.btnPot = new System.Windows.Forms.Button();
+            this.btnRaiz = new System.Windows.Forms.Button();
+            this.btnDer = new System.Windows.Forms.Button();
+            this.btnInt = new System.Windows.Forms.Button();
+            this.tb3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnLog = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // tb1
             // 
-            this.textBox1.Location = new System.Drawing.Point(13, 13);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(288, 22);
-            this.textBox1.TabIndex = 0;
+            this.tb1.Location = new System.Drawing.Point(46, 66);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(190, 22);
+            this.tb1.TabIndex = 0;
+            // 
+            // tb2
+            // 
+            this.tb2.Location = new System.Drawing.Point(320, 66);
+            this.tb2.Name = "tb2";
+            this.tb2.Size = new System.Drawing.Size(190, 22);
+            this.tb2.TabIndex = 1;
+            // 
+            // tbRes
+            // 
+            this.tbRes.Location = new System.Drawing.Point(590, 66);
+            this.tbRes.Name = "tbRes";
+            this.tbRes.ReadOnly = true;
+            this.tbRes.Size = new System.Drawing.Size(153, 22);
+            this.tbRes.TabIndex = 2;
+            // 
+            // btnSuma
+            // 
+            this.btnSuma.Location = new System.Drawing.Point(46, 134);
+            this.btnSuma.Name = "btnSuma";
+            this.btnSuma.Size = new System.Drawing.Size(139, 23);
+            this.btnSuma.TabIndex = 3;
+            this.btnSuma.Text = "+";
+            this.btnSuma.UseVisualStyleBackColor = true;
+            this.btnSuma.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnResta
+            // 
+            this.btnResta.Location = new System.Drawing.Point(46, 185);
+            this.btnResta.Name = "btnResta";
+            this.btnResta.Size = new System.Drawing.Size(139, 23);
+            this.btnResta.TabIndex = 4;
+            this.btnResta.Text = "-";
+            this.btnResta.UseVisualStyleBackColor = true;
+            this.btnResta.Click += new System.EventHandler(this.btnResta_Click);
+            // 
+            // btnMult
+            // 
+            this.btnMult.Location = new System.Drawing.Point(46, 232);
+            this.btnMult.Name = "btnMult";
+            this.btnMult.Size = new System.Drawing.Size(139, 23);
+            this.btnMult.TabIndex = 5;
+            this.btnMult.Text = "x";
+            this.btnMult.UseVisualStyleBackColor = true;
+            this.btnMult.Click += new System.EventHandler(this.btnMult_Click);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Location = new System.Drawing.Point(46, 289);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(139, 23);
+            this.btnDiv.TabIndex = 6;
+            this.btnDiv.Text = "/";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click);
+            // 
+            // btnSin
+            // 
+            this.btnSin.Location = new System.Drawing.Point(209, 134);
+            this.btnSin.Name = "btnSin";
+            this.btnSin.Size = new System.Drawing.Size(145, 23);
+            this.btnSin.TabIndex = 7;
+            this.btnSin.Text = "sin";
+            this.btnSin.UseVisualStyleBackColor = true;
+            this.btnSin.Click += new System.EventHandler(this.btnSin_Click);
+            // 
+            // btnCos
+            // 
+            this.btnCos.Location = new System.Drawing.Point(209, 185);
+            this.btnCos.Name = "btnCos";
+            this.btnCos.Size = new System.Drawing.Size(145, 23);
+            this.btnCos.TabIndex = 8;
+            this.btnCos.Text = "cos";
+            this.btnCos.UseVisualStyleBackColor = true;
+            this.btnCos.Click += new System.EventHandler(this.btnCos_Click);
+            // 
+            // btnTan
+            // 
+            this.btnTan.Location = new System.Drawing.Point(209, 232);
+            this.btnTan.Name = "btnTan";
+            this.btnTan.Size = new System.Drawing.Size(145, 23);
+            this.btnTan.TabIndex = 9;
+            this.btnTan.Text = "tan";
+            this.btnTan.UseVisualStyleBackColor = true;
+            this.btnTan.Click += new System.EventHandler(this.btnTan_Click);
+            // 
+            // btnPot
+            // 
+            this.btnPot.Location = new System.Drawing.Point(209, 289);
+            this.btnPot.Name = "btnPot";
+            this.btnPot.Size = new System.Drawing.Size(145, 23);
+            this.btnPot.TabIndex = 10;
+            this.btnPot.Text = "^";
+            this.btnPot.UseVisualStyleBackColor = true;
+            this.btnPot.Click += new System.EventHandler(this.btnPot_Click);
+            // 
+            // btnRaiz
+            // 
+            this.btnRaiz.Location = new System.Drawing.Point(391, 185);
+            this.btnRaiz.Name = "btnRaiz";
+            this.btnRaiz.Size = new System.Drawing.Size(145, 23);
+            this.btnRaiz.TabIndex = 11;
+            this.btnRaiz.Text = "raíz";
+            this.btnRaiz.UseVisualStyleBackColor = true;
+            this.btnRaiz.Click += new System.EventHandler(this.btnRaiz_Click);
+            // 
+            // btnDer
+            // 
+            this.btnDer.Location = new System.Drawing.Point(391, 232);
+            this.btnDer.Name = "btnDer";
+            this.btnDer.Size = new System.Drawing.Size(145, 23);
+            this.btnDer.TabIndex = 12;
+            this.btnDer.Text = "d/dx";
+            this.btnDer.UseVisualStyleBackColor = true;
+            this.btnDer.Click += new System.EventHandler(this.btnDer_Click);
+            // 
+            // btnInt
+            // 
+            this.btnInt.Location = new System.Drawing.Point(391, 289);
+            this.btnInt.Name = "btnInt";
+            this.btnInt.Size = new System.Drawing.Size(145, 23);
+            this.btnInt.TabIndex = 13;
+            this.btnInt.Text = "Integral def.";
+            this.btnInt.UseVisualStyleBackColor = true;
+            this.btnInt.Click += new System.EventHandler(this.btnInt_Click);
+            // 
+            // tb3
+            // 
+            this.tb3.Location = new System.Drawing.Point(590, 290);
+            this.tb3.Name = "tb3";
+            this.tb3.Size = new System.Drawing.Size(137, 22);
+            this.tb3.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(587, 269);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 17);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "limite inferior:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnLog
+            // 
+            this.btnLog.Location = new System.Drawing.Point(391, 134);
+            this.btnLog.Name = "btnLog";
+            this.btnLog.Size = new System.Drawing.Size(145, 23);
+            this.btnLog.TabIndex = 16;
+            this.btnLog.Text = "log";
+            this.btnLog.UseVisualStyleBackColor = true;
+            this.btnLog.Click += new System.EventHandler(this.btnLog_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(757, 339);
+            this.Controls.Add(this.btnLog);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb3);
+            this.Controls.Add(this.btnInt);
+            this.Controls.Add(this.btnDer);
+            this.Controls.Add(this.btnRaiz);
+            this.Controls.Add(this.btnPot);
+            this.Controls.Add(this.btnTan);
+            this.Controls.Add(this.btnCos);
+            this.Controls.Add(this.btnSin);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnMult);
+            this.Controls.Add(this.btnResta);
+            this.Controls.Add(this.btnSuma);
+            this.Controls.Add(this.tbRes);
+            this.Controls.Add(this.tb2);
+            this.Controls.Add(this.tb1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Calculadora";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -55,7 +239,23 @@ namespace CalcuCientifica
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.TextBox tb2;
+        private System.Windows.Forms.TextBox tbRes;
+        private System.Windows.Forms.Button btnSuma;
+        private System.Windows.Forms.Button btnResta;
+        private System.Windows.Forms.Button btnMult;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.Button btnSin;
+        private System.Windows.Forms.Button btnCos;
+        private System.Windows.Forms.Button btnTan;
+        private System.Windows.Forms.Button btnPot;
+        private System.Windows.Forms.Button btnRaiz;
+        private System.Windows.Forms.Button btnDer;
+        private System.Windows.Forms.Button btnInt;
+        private System.Windows.Forms.TextBox tb3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLog;
     }
 }
 
--- CalcuCientifica/CalcuCientifica/Form1.cs
+++ CalcuCientifica/CalcuCientifica/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using org.matheval;
 
 namespace CalcuCientifica
 {
@@ -22,16 +23,324 @@ namespace CalcuCientifica
 
         }
 
-        public double Potencia(double bas, double pot)
+        private double Potencia(double bas, double pot)
         {
             double res = Math.Pow(bas, pot);
             return res;
         }
 
-        public double Raiz(double bas, double ind)
+        private double Raiz(double bas, double ind)
         {
             double res = Math.Pow(bas, 1 / ind);
             return res;
+        }
+
+        private double Suma(double n1, double n2)
+        {
+            return (n1 + n2);
+        }
+        private double Resta(double n1, double n2)
+        {
+            return (n1 - n2);
+        }
+        private double Mult(double n1, double n2)
+        {
+            return (n1 * n2);
+        }
+        private double Div(double n1, double n2)
+        {
+            return (n1 / n2);
+        }
+        private double Log(double num, double nbase)
+        {
+            return (Math.Log(num, nbase));
+        }
+        private double Seno(double num)
+        {
+            return (Math.Sin(num));
+        }
+        private double Coseno(double num)
+        {
+            return (Math.Cos(num));
+        }
+        private double Tangente(double num)
+        {
+            return (Math.Tan(num));
+        }
+        private decimal Derivar(String funcion, decimal num)
+        {
+            decimal h = Convert.ToDecimal(Math.Pow(10, -5));
+            decimal val1;
+            decimal val2;
+            Expression expr = new Expression();
+            expr.SetFomular(funcion);
+            expr.Bind("x", num + h);
+            val1 = expr.Eval<decimal>();
+            expr.Bind("x", num);
+            val2 = expr.Eval<decimal>();
+            decimal res = (val1 - val2) / h;
+            return res;
+        }
+        private double Integrar(String funcion, double limSup, double limInf)
+        {
+            double val;
+            Expression expr = new Expression();
+            expr.SetFomular(funcion);
+            int nTriangulos = 1000;
+            double delta_x = (limSup - limInf) / nTriangulos;
+            double sumAc = 0;
+            for(int i = 1;i < nTriangulos + 1; i++)
+            {
+                expr.Bind("x", limInf + (i - 1) * delta_x);
+                val = expr.Eval<double>();
+                sumAc += (double)val * delta_x;
+            }
+            return sumAc;
+        }
+        private bool VerifTb()
+        {
+            bool res = false;
+            if (double.TryParse(tb1.Text, out _) && double.TryParse(tb2.Text,out _))
+            {
+                res = true;
+            }
+            return res;
+        }
+        private bool VerifTb1()
+        {
+            bool res = false;
+            if (double.TryParse(tb1.Text, out _))
+            {
+                res = true;
+            }
+            return res;
+        }
+        private bool VerifTbInt()
+        {
+            bool res = false;
+            if (double.TryParse(tb2.Text, out _) && double.TryParse(tb3.Text, out _))
+            {
+                res = true;
+            }
+            return res;
+        }
+        private bool VerifTbDer()
+        {
+            bool res = false;
+            if (decimal.TryParse(tb2.Text, out _))
+            {
+                res = true;
+            }
+            return res;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String res = "";
+            if (VerifTb())
+            {
+                res = Suma(Convert.ToDouble(tb1.Text), Convert.ToDouble(tb2.Text)).ToString();
+            }
+            else
+            {
+                res = "Syntax error";
+            }
+            tbRes.Text = res;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnResta_Click(object sender, EventArgs e)
+        {
+            String res = "";
+            if (VerifTb())
+            {
+                res = Resta(Convert.ToDouble(tb1.Text), Convert.ToDouble(tb2.Text)).ToString();
+            }
+            else
+            {
+                res = "Syntax error";
+            }
+            tbRes.Text = res;
+        }
+
+        private void btnMult_Click(object sender, EventArgs e)
+        {
+            String res = "";
+            if (VerifTb())
+            {
+                res = Mult(Convert.ToDouble(tb1.Text), Convert.ToDouble(tb2.Text)).ToString();
+            }
+            else
+            {
+                res = "Syntax error";
+            }
+            tbRes.Text = res;
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            String res = "";
+            if (VerifTb())
+            {
+                if(Convert.ToDouble(tb2.Text) == 0)
+                {
+                    res = "Math Error";
+                }
+                else
+                {
+                    res = Div(Convert.ToDouble(tb1.Text), Convert.ToDouble(tb2.Text)).ToString();
+                }
+            }
+            else
+            {
+                res = "Syntax error";
+            }
+            tbRes.Text = res;
+        }
+
+        private void btnSin_Click(object sender, EventArgs e)
+        {
+            String res = "";
+            if (VerifTb1())
+            {
+                res = Seno(Convert.ToDouble(tb1.Text)).ToString();
+            }
+            else
+            {
+                res = "Syntax error";
+            }
+            tbRes.Text = res;
+        }
+
+        private void btnCos_Click(object sender, EventArgs e)
+        {
+            String res = "";
+            if (VerifTb1())
+            {
+                res = Coseno(Convert.ToDouble(tb1.Text)).ToString();
+            }
+            else
+            {
+                res = "Syntax error";
+            }
+            tbRes.Text = res;
+        }
+
+        private void btnTan_Click(object sender, EventArgs e)
+        {
+            String res = "";
+            if (VerifTb1())
+            {
+                if (Convert.ToDouble(tb1.Text) == (Math.PI / 2))
+                {
+                    res = "Math Error";
+                }
+                else
+                {
+                    res = Tangente(Convert.ToDouble(tb1.Text)).ToString();
+                }
+                
+            }
+            else
+            {
+                res = "Syntax error";
+            }
+            tbRes.Text = res;
+        }
+
+        private void btnLog_Click(object sender, EventArgs e)
+        {
+            String res = "";
+            if (VerifTb())
+            {
+                if (Convert.ToDouble(tb1.Text) <= 0)
+                {
+                    res = "Math Error";
+                }
+                else
+                {
+                    res = Log(Convert.ToDouble(tb1.Text), Convert.ToDouble(tb2.Text)).ToString();
+                }
+            }
+            else
+            {
+                res = "Syntax error";
+            }
+            tbRes.Text = res;
+        }
+
+        private void btnPot_Click(object sender, EventArgs e)
+        {
+            String res = "";
+            if (VerifTb())
+            {
+                if (Convert.ToDouble(tb2.Text) == 0)
+                {
+                    res = "Math Error";
+                }
+                else
+                {
+                    res = Potencia(Convert.ToDouble(tb1.Text), Convert.ToDouble(tb2.Text)).ToString();
+                }
+            }
+            else
+            {
+                res = "Syntax error";
+            }
+            tbRes.Text = res;
+        }
+
+        private void btnRaiz_Click(object sender, EventArgs e)
+        {
+            String res = "";
+            if (VerifTb())
+            {
+                if (Convert.ToDouble(tb2.Text) == 0)
+                {
+                    res = "Math Error";
+                }
+                else
+                {
+                    res = Raiz(Convert.ToDouble(tb1.Text), Convert.ToDouble(tb2.Text)).ToString();
+                }
+            }
+            else
+            {
+                res = "Syntax error";
+            }
+            tbRes.Text = res;
+        }
+
+        private void btnDer_Click(object sender, EventArgs e)
+        {
+            String res = "";
+            if (VerifTbDer())
+            {
+                res = Derivar(tb1.Text, Convert.ToDecimal(tb2.Text)).ToString();
+            }
+            else
+            {
+                res = "Syntax Error";
+            }
+            tbRes.Text = res;
+        }
+
+        private void btnInt_Click(object sender, EventArgs e)
+        {
+            String res = "";
+            if (VerifTbInt())
+            {
+                res = Integrar(tb1.Text, Convert.ToDouble(tb2.Text), Convert.ToDouble(tb3.Text)).ToString();
+            }
+            else
+            {
+                res = "Syntax Error";
+            }
+            tbRes.Text = res;
         }
     }
 }
